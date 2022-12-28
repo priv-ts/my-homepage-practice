59d1a0514eaec04c4d1b2b1003015ce5d4739865
privts <priv.tomokisato@gmail.com>
2022-12-28 22:11:35 +0900

    disabledに対応したボタンのレイアウトを調整した

--- button_layout/button_layout.js
+++ button_layout/button_layout.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const button = document.getElementById("button-id");
+const button = document.getElementById("disabled-button");
 button.addEventListener("click", function () {
   this.innerHTML = "クリック済みです!";
   this.setAttribute("disabled", true);
